d26317524bc0775a55100fc47128ecb12a3c3c7a
tpfei <tpfei609@163.com>
2016-10-13 22:37:30 +0800

    clone graph

---
+++ Clone Graph.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "./scanner.h"
+
+struct UndirectedGraphNode {
+    int label;
+    vector<UndirectedGraphNode *> neighbors;
+    UndirectedGraphNode(int x) : label(x) {};
+};
+
+//DFS 
+
+class Solution {
+public:
+    UndirectedGraphNode *cloneGraph(const UndirectedGraphNode *node) {
+        if (node == nullptr) return nullptr;
+        // key is original node, value is copied node 
+        unordered_map<const UndirectedGraphNode *,
+                            UndirectedGraphNode *> copied;
+        clone(node, copied);
+        return copied[node];
+    }
+
+private:
+    //DFS 
+     static UndirectedGraphNode* clone(const UndirectedGraphNode *node,
+                unordered_map<const UndirectedGraphNode *,
+                UndirectedGraphNode *> &copied) {
+         // a copy already exists 
+         if (copied.find(node) != copied.end()) return copied[node];
+
+         UndirectedGraphNode *new_node = new UndirectedGraphNode(node->label);
+         copied[node] = new_node;
+         for (auto nbr :　node->neighbors)
+             new_node->neighbors.push_back(clone(nbr, copied));
+         return new_node;
+     }
+
+};
